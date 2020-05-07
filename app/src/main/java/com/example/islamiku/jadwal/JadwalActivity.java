e2d91fba4174f1591ca0b40cf9c06dec68f4b1e7
Ryuze <alfianyunianto74@gmail.com>
2020-05-07 12:54:06 +0700

    update kedua

--- app/src/main/java/com/example/islamiku/jadwal/JadwalActivity.java
+++ app/src/main/java/com/example/islamiku/jadwal/JadwalActivity.java
@@ -12,6 +12,5 @@ public class JadwalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwal);
-        getSupportActionBar().setTitle("Jadwal Sholat");
     }
 }
